f4bbd9360774bba573f042f87fe9f8af649df274
Klauer - Nick Klauer <nicholasklauer@alliantenergy.com>
2011-04-03 06:24:40 -0500

    Saving progress.
    Still trying to figure out how to properly convert BigDecimal to 2 decimal
    places for all calcluations, but I might not need to worry about this detail
    as much as I thought since the result would likely NOT end in .00 or .x0, and
    if it did, I can change the output to add another `0` to the result.

--- 1-international_trade/lib/rates.rb
+++ 1-international_trade/lib/rates.rb
@@ -1,19 +1,6 @@
-=begin
-some thoughts on this exercise:
-
-Rates.xml file is pretty simple, and I could probably
-use a hashmap to map all currencies to USD.  For currencies that are not 1-1 from something-USD, I can def that out
-
-def to_usd(currency)
-  # find direct mapping
-  # if none exist, find any mapping
-  # tree downwards
+unless RUBY_PLATFORM != "java"
+  gem "nokogiri", "=1.5.0.beta.4" # earlier version threw Java hardcore errors
 end
-
-? what is the storeid there for?  Is that a hint to try using that store mapping to simplify things?  I think it might be, otherwise it's just superfluous.
-
-=end
-gem "nokogiri", "=1.5.0.beta.4" # earlier version threw Java hardcore errors
 require 'nokogiri'
 
 require 'fastercsv'
@@ -37,6 +24,11 @@ class CsvTransactionParser
     @arrs.select { |ary| ary[0] == store }
   end
 
+  def retrieve_trans(store_name)
+    ars = retrieve_stores(store_name)
+    ars.map { |a| a[2] }
+  end
+
 end
 
 class XmlRatesParser
@@ -52,7 +44,12 @@ class XmlRatesParser
   # to data[from]
   def initialize(filepath)
     @doc = create_doc(filepath)
-    @rates_xml = get_rates_from_xml(@doc)
+    create_rates
+  end
+
+  def create_rates
+    xml = get_rates_from_xml(@doc)
+    @rates = map_rates(xml)
   end
 
   def create_doc(filepath)
@@ -60,24 +57,48 @@ class XmlRatesParser
   end
 
   def get_rates_from_xml(doc)
-    doc.xpath('//rate')
+    doc.css('rate')
   end
 
-  def map_rate(xml_element, hash={})
-    puts "In XmlRatesParser; calling map_rate. #{xml_element.class}"
-    hash[xml_element['from']] = { 'to' => xml_element['to'],
-                                  'conversion' => xml_element['conversion'] }
+  def map_rate(element, hash={})
+    hash[element.css('from').text] = { 'to' => element.css( 'to' ).text,
+                                       'conversion' => element.css( 'conversion' ).text }
+    hash
   end
 
+  def map_rates(xml)
+    rates = {}
+    xml.each { |r|
+      map_rate(r, rates)
+    }
+    rates
+  end
 
 end
 
 class Banker
 
+  attr_accessor :rates, :transactions, :xml_parser, :csv_parser
 
-  
+  def initialize(rate_file, transaction_file)
+    @xml_parser = XmlRatesParser.new rate_file
+    @csv_parser = CsvTransactionParser.new transaction_file
+  end
+
   def round_banker_style(value)
-    BigDecimal(value.to_s).round(2, BigDecimal::ROUND_HALF_EVEN).to_f
+    BigDecimal(value.to_s).round(2, BigDecimal::ROUND_HALF_EVEN).to_s(2)
+  end
+
+  def convert(amt, currency)
+    unless currency == "USD"
+      conversion = find_conversion_amt(currency, "USD")
+    else
+      conversion = 1 
+    end
+    round_banker_style(amt * conversion)
+  end
+
+  def find_conversion_amt(from, to)
   end
 
 end
--- 1-international_trade/spec/rates_spec.rb
+++ 1-international_trade/spec/rates_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Parsing Rates.XML" do
+describe "XML Parser" do
   
   before(:each) do
     @xml_parser = XmlRatesParser.new('files/SAMPLE_RATES.xml')
@@ -8,24 +8,23 @@ describe "Parsing Rates.XML" do
   end
 
   it "should get rates XML from a document" do
-    @xml_parser.get_rates_from_xml(@doc)
+    rates = @xml_parser.get_rates_from_xml(@doc)
+    rates.size.should == 3
+    rates.class.should == Nokogiri::XML::NodeSet
   end
 
   it "should map a 'from' conversion to a 'to' and 'conversion'" do
-    r = @xml_parser.map_rate(@doc.xpath('//rate'))
+    r = @xml_parser.map_rate(@doc.css('rate').first)
     r.class.should == Hash
     puts r['AUD']
     r['AUD']['to'].should == "CAD"
     r['AUD']['conversion'].should == "1.0079"
   end
 
-  it "should know how to convert the reverse of a conversion"
-
-  it "should find a conversion that isn't directly mapped"
 
 end
 
-describe "parsing trans.csv" do
+describe "CSV Parser" do
   
   before(:each) do
     @csv = CsvTransactionParser.new('files/SAMPLE_TRANS.csv')
@@ -45,34 +44,56 @@ describe "parsing trans.csv" do
     @csv.retrieve_skus("DM1302").size.should == 0
   end
 
-end
-
-describe "converting prices to USD" do
-  before(:each) do
-    @banker = Banker.new
+  it "should have a list of all transactions for a store" do
+    yon = @csv.retrieve_trans("Yonkers")
+    yon.size.should == 2
+    yon[0].should == "70.00 USD"
+    @csv.retrieve_trans("Nashua").size.should == 1
+    @csv.retrieve_trans("Scranton").size.should == 1
+    @csv.retrieve_trans("Camden").size.should == 1
+    @csv.retrieve_trans("MadeUp").size.should == 0
   end
-
-  it "should have a mapping of all of the expenses"
-
-  it "should maintain a running total in USD for all stores"
 end
 
-describe "output.txt file" do
+describe "Banker" do
   
-  describe "and bankers rounding" do
+  describe "and rounding" do
+    before(:each) do
+      @rate_f = "files/SAMPLE_RATES.xml"
+      @trans_f = "files/SAMPLE_TRANS.csv"
+      @bank = Banker.new(@rate_f, @trans_f)
+    end
     
     it "should round various decimals to proper bankers rounding format" do
-      bank = Banker.new
-      bank.round_banker_style("1.45453626").should == 1.45
-      bank.round_banker_style("1.46456622").should == 1.46
-      bank.round_banker_style("1.44653426").should == 1.45
-      bank.round_banker_style(1.4562722).should == 1.46
+      @bank.round_banker_style("1.45453626").should == "1.45"
+      @bank.round_banker_style("1.46456622").should == "1.46"
+      @bank.round_banker_style("1.44653426").should == "1.45"
+      @bank.round_banker_style(1.4562722).should == "1.46"
+      @bank.round_banker_style("101.69711").should == "101.70"
+      @bank.round_banker_style("70.00").should == "70.00"
+    end
+
+    describe "with conversions" do
+
+      it "should not calculate the value of anything passed in as USD" do
+        @bank.convert("70.00", "USD").should == "70.00"
+      end
+
+      it "should calculate foreign currencies to USD" do
+        @bank.convert("70.00", "CAD").should == "70.63"
+        @bank.convert("100.00", "AUD").should == "101.70"
+      end
+
     end
 
   end
 
-  it "should use bankers rounding for each result"
+  it "should result in a single total number for a given sku"
 
-  it "should avoid floating point arithmetic errors"
+  it "should know how to convert from one currency to USD"
+
+  it "should know how to convert the reverse of a conversion"
+
+  it "should find a conversion that isn't directly mapped"
 
 end
