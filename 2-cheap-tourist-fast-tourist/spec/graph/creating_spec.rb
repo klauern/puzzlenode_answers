e7e5dcd6236e8aa2a6ea43d62ca064cd29be59ef
Nick Klauer <klauer@gmail.com>
2012-04-29 06:21:00 -0500

    airport consistency

--- 2-cheap-tourist-fast-tourist/spec/graph/creating_spec.rb
+++ 2-cheap-tourist-fast-tourist/spec/graph/creating_spec.rb
@@ -32,4 +32,17 @@ describe "creating a graph from a file" do
     flight[:cost].must_be(:==, 100)
     flight[:flight_time].must_be(:==, 2)
   end
+
+
+  it "should NOT create multiple airports given multiple input lines" do
+    graph = FlightGraph.new
+
+    ["A C 17:00 18:07 161.80", "C D 14:00 14:42 656.71", "D Z 21:00 22:07 312.80",
+             "C D 01:00 01:42 90.71"].each { |flight|
+      graph.read_flight(flight)
+    }
+    %w{ A C D Z }.each { |a|
+      Airport.find("name: #{a}").size.must_be(:==, 1)
+    }
+  end
 end
